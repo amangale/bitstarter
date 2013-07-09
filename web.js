339093543ac8652d125d2186746e6304bf00fff0
Ubuntu <ubuntu@ip-172-31-18-205.us-west-2.compute.internal>
2013-07-09 09:38:20 +0000

    modified web.js .

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send((fs.readFileSync('./index.html').toString('utf-8'));
+  response.send((fs.readFileSync('/home/ubuntu/bitstarter/index.html')).toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
